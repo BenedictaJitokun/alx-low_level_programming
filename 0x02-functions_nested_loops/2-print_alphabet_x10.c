7428af8db5ed67a2b22239f068e6d1ffa77ba2e6
BenedictaJitokun <jitokunovakporhiye2@gmail.com>
2022-12-13 04:31:58 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints from a to z 10x
+ */
+void print_alphabet_x10(void)
+{
+	char ch = 'a';
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n')
+}
+
